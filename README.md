a76e65f6a5b44d9fb66d43ca00333e472af1d354
Lomax12436 <110442488+Lomax12436@users.noreply.github.com>
2023-03-08 17:40:59 -0800

    I believe I got it working

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # Weather-Man-App
+THis homework will deliver a fully functioning weather dashboard thzt utlizes a dynamically updating content page, saved search history, and location autocomplete. It will allow practice with using third party APIs and Jquery manipulation of the DOM.
+
+# Code Style
+This project is written using Java script and the Bootstrap CDN for layout and mobile responsiveness. Jquery is used for selectors and manipulation of the Dom. The OpenWeather API is used to pull current and future weather data and Google's Maps API is used for both geo conversion and auto completion of typed locations. Moment.js calls are made to get the current time and convert the returned UTC time into a easily read format.
+
+# How to Use
+Enter any address with as much or as little detail as required. Click the submit button to load the results. Click any saved history button to generate the current information for the previous city.
---
+++ assets/javascript.js
@@ -0,0 +1,206 @@
+//Initial variable declaration
+var weatherOneCallQueryBaseUrl = "https://api.openweathermap.org/data/2.5/onecall?appid=cbd9375ec50e479521231c47f4b76ae3&units=imperial&lat="
+var geocoder = new google.maps.Geocoder;
+var place = "";
+var cityNameDisplay = "";
+var currentCityData = [];
+var currentCityGeo = [];
+var searchedCities = [];
+
+//Makes API calls. Requires a query URL and a function to run when complete
+function apiCall(apiQuery, apiFunction) {
+    $.ajax({
+        url: apiQuery,
+        method: "GET"
+    }).then(apiFunction)
+}
+
+//Loads Google Places search and autocomplete functionality
+function initialize() {
+    var input = document.getElementById('citySearchBox');
+    new google.maps.places.Autocomplete(input);
+}
+
+//Searches for city from search box or clicked button from history
+function searchCity(history) {
+    if (history == null) {
+        place = $('#citySearchBox').val();
+    }
+    else {
+        place = history;
+    }
+    geocodeCity(place);
+}
+
+//Constructs the city name for display from information passed from weather API
+function buildCityName(geoInfo) {
+    cityNameDisplay = "";
+    //Flags city found as false in case just a state is entered and a state flag if just a country is entered.
+    var cityFound = false;
+    var stateFound = false;
+    for (i = 0; i < geoInfo.address_components.length; i++) {
+        if (geoInfo.address_components[i].types[0] == "locality") {
+            cityNameDisplay = cityNameDisplay + geoInfo.address_components[i].long_name + ", ";
+            var cityFound = true;
+        }
+        else if (geoInfo.address_components[i].types[0] == "administrative_area_level_1") {
+            cityNameDisplay = cityNameDisplay + geoInfo.address_components[i].short_name;
+            var stateFound = true;
+        }
+        else if (geoInfo.address_components[i].types[0] == "country" && cityFound == false && stateFound == true) {
+            cityNameDisplay = cityNameDisplay + ", " + geoInfo.address_components[i].short_name;
+        }
+        else if (stateFound == false && cityFound == false) {
+            cityNameDisplay = geoInfo.address_components[i].long_name;
+        }
+    }
+}
+
+//Converts entered city name using google geocode API for accurate lat and long
+function geocodeCity(location) {
+    geocoder.geocode({ 'address': location }, function (results, status) {
+        //Handler for gibberish entries into search box
+        if (status == google.maps.GeocoderStatus.OK) {
+            currentCityGeo = results[0];
+            var latitude = results[0].geometry.location.lat();
+            var longitude = results[0].geometry.location.lng();
+            var searchCityUrl = weatherOneCallQueryBaseUrl + latitude + "&lon=" + longitude;
+            apiCall(searchCityUrl, loadCurrent);
+        }
+    })
+}
+
+//Reads saved search history from local storage
+function loadSearchHistory() {
+    var savedHistory = localStorage.getItem('wdSearchHistory');
+    if (savedHistory == null) {
+        searchedCities = [];
+    }
+    else {
+        searchedCities = JSON.parse(savedHistory);
+        buildSearchButtons();
+        searchCity(searchedCities[searchedCities.length - 1]);
+
+    }
+}
+
+//Handles updating array of saved searches
+function updateSearchHistory(searchString) {
+    //Cyces through etries to see if they already exist.
+    for (i = 0; i < searchedCities.length; i++) {
+        if (searchedCities[i] == searchString) {
+            return;
+        }
+    }
+    //Caps limit at ten and removes first entry if greater than
+    if (searchedCities.length >= 10) {
+        searchedCities.shift();
+    }
+    //Adds new city to search history array. Converts for local storage and saves
+    searchedCities.push(searchString);
+    var searchedCitiesHolder = JSON.stringify(searchedCities);
+    localStorage.setItem('wdSearchHistory', searchedCitiesHolder);
+    buildSearchButtons();
+}
+
+//Builds search history buttons, assigns data value to store formatted name
+function buildSearchButtons() {
+    $('#searchHistoryList').empty();
+    for (i = searchedCities.length - 1; i >= 0; i--) {
+        var newSearchButton = $("<button type='button' class='list-group-item list-group-item-action' data='" + searchedCities[i] + "'> </button>");
+        newSearchButton.text(searchedCities[i]);
+        $('#searchHistoryList').append(newSearchButton);
+    }
+    $('.list-group-item-action').on('click', function () {
+        loadSearchButton($(this))
+    });
+}
+
+//Handles events for clicked search history buttons
+function loadSearchButton(response) {
+    var searchHistoryString = response.attr('data');
+    for (i = searchedCities.length - 1; i >= 0; i--) {
+        if (searchHistoryString == searchedCities[i]) {
+            var searchHistoryHolder = searchedCities[i];
+            for (x = i; x < searchedCities.length; x++) {
+                searchedCities[x] = searchedCities[x + 1];
+            }
+        }
+    }
+    searchedCities[searchedCities.length - 1] = searchHistoryHolder;
+    var searchedCitiesHolder = JSON.stringify(searchedCities);
+    localStorage.setItem('wdSearchHistory', searchedCitiesHolder);
+    buildSearchButtons();
+    searchCity(searchHistoryString);
+}
+
+//Dynamically updates the main content window with both current and five day forecast
+function loadCurrent(response) {
+    $('#fiveDayRow').removeAttr('hidden');
+    $('#citySearchBox').val("");
+    currentCityData = response;
+    buildCityName(currentCityGeo);
+    updateSearchHistory(cityNameDisplay);
+    cityNameDisplay = cityNameDisplay + " (" + moment().tz(currentCityData.timezone).format('l, h:mma') + ")";
+    $('#currentCityName').text(cityNameDisplay);
+    $('#currentCityIcon').attr("src", "http://openweathermap.org/img/wn/" + currentCityData.current.weather[0].icon + "@2x.png");
+    $('#currentCityIcon').removeAttr("hidden");
+    $('#currentCityTemperature').html("Temperature: " + currentCityData.current.temp + " &deg;F");
+    $('#currentCityHumidity').html("Humidity: " + currentCityData.current.humidity + "&percnt;");
+    $('#currentCityWindSpeed').html("Wind Speed: " + currentCityData.current.wind_speed + " MPH");
+    var loadCurrentUV = parseFloat(currentCityData.current.uvi);
+    $('#currentCityUV').html("UV Index: <span id='currentCityUVColor'>" + loadCurrentUV + "</span>");
+    //Handles color coding for UVI scale
+    if (loadCurrentUV < 3) {
+        var loadCurrentUVColor = "green";
+    }
+    else if (loadCurrentUV >= 3 && loadCurrentUV < 6) {
+        var loadCurrentUVColor = "yellow";
+    }
+    else if (loadCurrentUV >= 6 && loadCurrentUV < 8) {
+        var loadCurrentUVColor = "orange";
+    }
+    else if (loadCurrentUV >= 8 && loadCurrentUV < 11) {
+        var loadCurrentUVColor = "red";
+    }
+    else {
+        var loadCurrentUVColor = "purple";
+    }
+    $('#currentCityUVColor').attr("style", "background-color:" + loadCurrentUVColor);
+    //Cycles through fie days of data to build cards for forecast
+    for (i = 1; i < 6; i++) {
+        var forecastDay = "#day" + i;
+        var forecastData = currentCityData.daily[i];
+        $(forecastDay).children().children('.card-title').eq(0).html(moment(01 / 01 / 1970).add(forecastData.dt, 'seconds').format('l'))
+        var forecastIcon = forecastData.weather[0].icon;
+        $(forecastDay).children().children('img').eq(0).attr('src', "http://openweathermap.org/img/wn/" + forecastIcon + "@2x.png")
+        $(forecastDay).children().children('p').eq(0).html("Temp: " + forecastData.temp.max + " &deg;F");
+        $(forecastDay).children().children('p').eq(1).html("Humidity: " + forecastData.humidity + "&percnt;");
+
+    }
+}
+
+//Event listener for search box button
+$('#citySearchButton').on('click', function () {
+    event.preventDefault();
+    searchCity();
+});
+
+$('#clearHistoryConfirm').on('click', function () {
+    event.preventDefault();
+    if (searchedCities.length === 0) {
+    }
+    else {
+        searchedCities = [];
+        localStorage.removeItem('wdSearchHistory');
+        buildSearchButtons();
+    }
+    $('#clearHistoryModal').modal('hide');
+
+
+})
+
+//initiliazes google maps autocomplete
+google.maps.event.addDomListener(window, 'load', initialize);
+loadSearchHistory();
+;
